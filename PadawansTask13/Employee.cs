6e8ec37cf5bcbd941b319dd9a3ee475a6972dd07
Marina Sokolova <sokolova.m.e9@gmail.com>
2019-04-19 10:29:22 +0300

    add padawanstasks

---
+++ PadawansTask13/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PadawansTask13
+{
+    class Employee
+    {
+        string surname;
+        private int age;
+
+        public Employee()
+        {
+            surname = string.Empty;
+        }
+        public Employee(string surname, int age)
+        {
+            this.surname = surname;
+            this.age = age;
+        }
+
+        private string ReturnAgeValue()
+        {
+            return age.ToString();
+        }
+
+        public void ChangeSurname(string newsurname)
+        {
+            surname = newsurname; 
+        }
+
+        public string ReturnEmployeeInfo()
+        {
+            string info = String.Concat("Surname: ", surname, ", Age: ", age);
+            return info;
+        }
+    }
+}
---
+++ PadawansTask9/CartesianCoordinates.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PadawansTask9
+{
+    public class CartesianCoordinates
+    {
+        public static List<Point> GetNeighbors(Point point, int range, params Point[] points)
+        {
+            // put your code here
+            //throw new NotImplementedException();
+            if (points == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if(range <= 0)
+            {
+                throw new ArgumentException();
+            }
+            List<Point> resultList = new List<Point>();
+            for (int i = 0; i < points.Length; i++)
+            {
+                if (Math.Abs(points[i].X - point.X) <= range && Math.Abs(points[i].Y - point.Y) <= range)
+                {
+                    resultList.Add(points[i]);
+                }
+            }
+            return resultList;
+        }
+    }
+}
